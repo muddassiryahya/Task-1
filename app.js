a8531839b71558771aa3261afa9942e328f5a27a
muddassiryahya <m.m2442212@gmail.com>
2023-12-01 21:00:07 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// var userName = prompt("Enter your name")
+
+// if (!userName) {
+//     alert(" Error please fill in the input")
+// } else {
+//     var phoneNumber = +prompt("Enter your phone number")
+// }
+
+// if (!phoneNumber) {
+//     alert(" Error please fill in the input")    
+// } else {
+//     var email = prompt("Enter your email address")
+// }
+
+// if (!email) {
+//     alert(" Error please fill in the input")    
+// } else {
+//     var password = prompt("Enter your password")
+// }
+
+// if (!password) {
+//     alert(" Error please fill in the input")    
+// }  else {
+//     var conformPassword = prompt("Enter your conform password")
+// }
+
+// if (conformPassword === password){
+//     alert("You are successfully registered")
+// } else {
+//     alert(" Password should be same")
+// }
+
+
+
+
+// loop
+
+// var table = "2"
+
+// for (i=1; i<=10; i++) {
+//     console.log(table + " * " + i + " = " + table*i)
+// }
+
+
+
+// var students = ["ali","ahmed","kashif","maaz","hammad"]
+
+// for (i=0; i < students.length  ; i++) {
+//     console.log("iteration===>", i , students[i])
+// }
+
